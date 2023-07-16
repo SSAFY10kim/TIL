770778915e0288e678b03787751e57a9dd0a6ab9
SSAFY10kim <139415938+SSAFY10kim@users.noreply.github.com>
2023-07-16 21:44:14 +0900

    2001 clear

--- 알고리즘/clear/2001.py
+++ 알고리즘/clear/2001.py
@@ -1,6 +1,6 @@
-import sys
+# import sys
 
-sys.stdin = open(r'C:\Users\PC1\Desktop\TIL\알고리즘\input.txt','r')
+# sys.stdin = open(r'C:\Users\HOME\Desktop\practice\input.txt','r')
 
 T = int(input())
 
@@ -8,19 +8,20 @@ for t in range(T):
     n, m = map(int, input().split())
 
     matrix = [list(map(int, input().split())) for _ in range(n)]            # 2중 리스트 입력받는법 항상 기억해둘것
-
+    
     kill_number = []
     for i in range(n):
         for j in range(n):
             kill = []
-            point = matrix[i][j]
+            # print(point)
 
-            for row in range(1,m-1):
-                for col in range(1,m-1):
-                    if (i + row) > (n - 1) or  (j + col) > (n - 1):
+            for row in range(0,m):                                          # row +
+                for col in range(0,m):                                      # col +
+                    if (i + row) > (n - 1) or  (j + col) > (n - 1):         # 초과 범위 제한
                         continue
+                    # print(matrix[i+row][j+col])
                     kill.append(matrix[i+row][j+col])
-        print(kill)
-        kill_number.append(sum(kill) + point)
+            # print(kill)
+            kill_number.append(sum(kill))
     
     print("#{} {}" .format(t+1, max(kill_number)))
