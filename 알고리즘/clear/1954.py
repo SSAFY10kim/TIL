75465cce02d812a14b57cfb258e52922bb07dd66
SSAFY10kim <ehddud0534@gmail.com>
2023-07-16 16:19:28 +0900

    clear 1954

--- 알고리즘/clear/1954.py
+++ 알고리즘/clear/1954.py
@@ -19,32 +19,32 @@ for t in range(T):
         if count == 0:
             break
         
-        for _ in range(count):
-            goal_mat[row][col] = num
+        for _ in range(count):          # 아래진행
+            goal_mat[row][col] = num 
             num += 1
             row += 1
-        for _ in range(count):
+        for _ in range(count):          # 좌측진행
             goal_mat[row][col] = num
             num += 1
             col -= 1
         
-        if count - 1 == 0:
+        if count - 1 == 0:              # 범위 0이면 탈출
             goal_mat[row][col] = num
             break
         
-        for _ in range(count-1):
+        for _ in range(count-1):        # 위쪽진행
             goal_mat[row][col] = num
             num += 1
             row -= 1
-        for _ in range(count-1):
+        for _ in range(count-1):        # 우측진행
             goal_mat[row][col] = num
             num += 1
             col += 1
         
-        goal_mat[row][col] = num
+        goal_mat[row][col] = num        # 탈출시 탈출
 
     print("#{}" .format(t+1))
-    for i in goal_mat:
+    for i in goal_mat:                  # 리스트 풀어서 출력하기
         for j in i:
             print(j, end=' ')
         print()
