16451fc8e00f91f1e9cb2503436dfcd5be59eac4
SSAFY10kim <ehddud0534@gmail.com>
2023-07-17 22:21:51 +0900

    11

--- 알고리즘/practice.py
+++ 알고리즘/practice.py
@@ -1,47 +1,18 @@
-import sys
+number_of_people = 0
 
-sys.stdin = open(r'C:\Users\SSAFY\Desktop\TIL\알고리즘\input.txt','r')
+def increase_user():
+    global number_of_people
+    number_of_people += 1
+    print(f"현재 가입 된 유저 수 : {number_of_people}")
 
-T = int(input())
+def create_user(name, age, address):
+    global number_of_people
+    print(f"현재 가입 된 유저 수 : {number_of_people}")              # 초기 인원 print
+    user_info = dict(name = name, age = age, address = address)    # 딕셔너리에 각 값 입력 
+    print(f"{name}님 환영합니다!")                                  # 이름 출력
+    print(user_info)                                               # 딕셔너리 출력
+    increase_user()                                                # 유저 수 += 1
 
-for t in range(T):
-    n, m = map(int, input().split())
-
-    matrix = [list(map(int, input().split())) for _ in range(n)]
-
-    # print(matrix)           # 입력값 검증
-
-    ans = 0
-
-    for i in range(n):
-        for j in range(n):
-            count = 0
-            pointer = matrix[i][j]        # 포인터 이동
-            if pointer == 1:
-                for right in range(n):
-                    if (j + right) > n-1:
-                        continue
-
-                    if matrix[i][j + right] == 1:
-                        count += 1
-                    else:
-                        break
-                if count == m:
-                    ans += 1
-                    count = 0
-                
-                
-                for down in range(n):
-                    if (i + down) > n-1:
-                        continue
-
-                    if matrix[i+down][j] == 1:
-                        count += 1
-                    else:
-                        break
-                if count == m:
-                    ans += 1
-                    
-                    
-    print(f'#{t+1} {ans}')
-                    
+create_user("홍길동", 30, "서울")                                   # 각 인자 입력
+    
+    
