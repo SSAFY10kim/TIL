06f300e88ccfd791f035abd28b3d9c211200eec3
SSAFY10kim <ehddud0534@gmail.com>
2023-07-18 17:39:14 +0900

    0718 end

--- 알고리즘/ssafygit/ws_3_3.py
+++ 알고리즘/ssafygit/ws_3_3.py
@@ -1,10 +1,24 @@
-from book import decrease_book
-number_of_book = 100
+import requests
+from pprint import pprint as print
 
-# ws_3_3.py
-def rental_book(name=str, num=int):
-    global number_of_book
-    decrease_book(num)
-    print(f"{name}님이 {num}권의 책을 대여하였습니다.")
+dummy_data = []
+for i in range(1,11):
+    # 무작위 유저 정보 요청 경로
+    API_URL = (f'https://jsonplaceholder.typicode.com/users/{i}')
+    # API 요청
+    response = requests.get(API_URL)
+    # JSON -> dict 데이터 변환
+    parsed_data = response.json()
+    # # 응답 데이터 출력
+    # print(response)
 
-rental_book("홍길동",3)
+    # # 변환 데이터 출력
+    # print(parsed_data)
+    # # 변환 데이터의 타입
+    # print(type(parsed_data))
+    
+    dummy_data.append(parsed_data["name"])
+
+# 특정 데이터 출력
+
+print(dummy_data)
