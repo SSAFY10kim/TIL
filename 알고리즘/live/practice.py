0799fab403c7ca5d706c8d0f8be831a61107e27b
SSAFY10kim <ehddud0534@gmail.com>
2023-07-29 22:51:28 +0900

    11

--- 알고리즘/live/practice.py
+++ 알고리즘/live/practice.py
@@ -1,32 +1,31 @@
-class Person:
-    gene = 'XYZ'
-    
-    def __init__(self, name):
-        self.name = name
+# hw_8_4.py
 
-    def greeting(self):
-        return f'안녕, {self.name}'
+# 아래 클래스를 수정하시오.
+class UserInfo:
+    def __init__(self):
+        self.user_data = {}
     
-class Mom(Person):
+    def get_user_info(self):
+        while True:
+            try:
+                user_name = input("이름을 입력하세요: ")
+                user_age = int(input("나이를 입력하세요: "))
+                self.user_data['이름'] = user_name
+                self.user_data['나이'] = user_age
+                break
+                
+            except:
+                print("나이는 숫자로 입력해야 합니다.")
+                return False
 
-    def swim(self):
-        return '엄마가 수영'
-    
-class Dad(Person):
+    def display_user_info(self):
+        if '이름' in self.user_data and '나이' in self.user_data:
+            print("사용자 정보:")
+            print(f"이름: {self.user_data['이름']}")
+            print(f"나이: {self.user_data['나이']}")
+        else:
+            print('사용자 정보가 입력되지 않았습니다.')
 
-    def walk(self):
-        return '아빠가 걷기'
-    
-class FirstChild(Dad, Mom): # 클래스 변수 X, 부모class꺼 받아옴
-    def swim(self):         # 재정의, 덮어쓰기?
-        return '첫째가 수영'
-    
-    def cry(self):          # 없던거, 본인 메서드
-        return '첫째가 응애'
-    
-baby1 = FirstChild('아가')
-print(baby1.cry())  # 첫째가 응애
-print(baby1.swim()) # 첫째가 수영
-print(baby1.walk()) # 아빠가 걷기
-print(baby1.gene)   # XY    상속 순서가 Dad,Mom 이니까 첫번째 클래스의 변수 받아옴
-print(FirstChild.mro())
+user = UserInfo()
+user.get_user_info()
+user.display_user_info()
